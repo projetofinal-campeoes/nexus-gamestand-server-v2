e4b95124b0c45c96d612274ab22ce6e893a06c32
Marchi8 <luandeoliveirasoares@gmail.com>
2023-01-04 10:00:28 -0300

    feat: permissions

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .permissions import AccountOwner
 
 
 class UserView(ListCreateAPIView):
@@ -11,5 +12,6 @@ class UserView(ListCreateAPIView):
 
 class UserDetailView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
+    permission_classes = [AccountOwner]
     queryset = User.objects.all()
     serializer_class = UserSerializer
