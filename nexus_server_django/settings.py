26898fd53bb8c229ba7c155f5cec35143452ced4
mathsudre <mathsudre@gmail.com>
2023-01-04 15:52:16 -0300

    feat: change readme

--- nexus_server_django/settings.py
+++ nexus_server_django/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": "127.0.0.1",
         "PORT": 5432,
-    }
+    },
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
