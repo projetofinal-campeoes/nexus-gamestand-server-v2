72a28fa141c07ae9eab75765b28d331ea962f96b
vagnermengali <vagmengali2015@gmail.com>
2023-01-05 13:40:37 -0300

    feat: adding refactoring to custom game

--- custom_games/permissions.py
+++ custom_games/permissions.py
@@ -1,6 +1,7 @@
-from rest_framework import permissions
 from .models import CustomGames
 from rest_framework.views import View
+from rest_framework import permissions
+
 
 class IsAccountOwner(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: CustomGames) -> bool:
