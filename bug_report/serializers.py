1d0a32e86d7222b7ebe1abbd1cd81eaf28f30141
yancarlodev <yancarlodc@gmail.com>
2023-01-09 14:10:13 -0300

    feat: add login and register to options on pages and now the returns user_id

--- bug_report/serializers.py
+++ bug_report/serializers.py
@@ -4,4 +4,4 @@ from .models import BugReport
 class BugReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = BugReport
-        fields = ['id', 'page', 'description', 'created_at']
+        fields = ['id', 'page', 'description', 'created_at', 'user_id']
