6bda4dcc3675ea443b8d86d44cc94b8c74c6703c
ssidartaa <odssy351@gmail.com>
2023-01-28 20:14:31 -0500

    feat: deploy settings

--- bug_report/models.py
+++ bug_report/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 import uuid
 
+
 class PageChoices(models.TextChoices):
-    LOGIN = 'Login'
-    REGISTER = 'Register'
-    HOME = 'Home'
-    PROFILE = 'Profile'
-    GAMES = 'Games'
-    PROMOTIONS = 'Promotions'
-    FRIENDS = 'Friends'
-    SETTINGS = 'Settings'
-    BUG_REPORT = 'Bug report'
+    LOGIN = "Login"
+    REGISTER = "Register"
+    HOME = "Home"
+    PROFILE = "Profile"
+    GAMES = "Games"
+    PROMOTIONS = "Promotions"
+    FRIENDS = "Friends"
+    SETTINGS = "Settings"
+    BUG_REPORT = "Bug report"
+
 
 class BugReport(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    page = models.CharField(max_length=20, choices=PageChoices.choices, default=PageChoices.HOME)
+    page = models.CharField(
+        max_length=20, choices=PageChoices.choices, default=PageChoices.HOME
+    )
     description = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='bugs_report')
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="bugs_report"
+    )
